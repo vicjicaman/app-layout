6e5c0aeaee4b90c8094d12ef3bf179e75c6d3318
Victor Jimenez <vic.jicama@gmail.com>
2019-03-03 17:04:37 -0600

    Improve the menu layout and add code-fork logo

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,8 +5,8 @@ import {Footer} from './Footer.jsx';
 import {Sidebar} from './Sidebar.jsx';
 import {Indice} from './Indice.jsx';
 
-export const Layout = ({children, brand, rightMenu, leftMenu, indices}) => <div>
-  <Menu brand={brand} right={rightMenu} left={leftMenu}/>
+export const Layout = ({children, brand, rightMenu, leftMenu, content,  indices}) => <div>
+  <Menu brand={brand} right={rightMenu} left={leftMenu} content={content}/>
   <div style={{
       marginBottom: "90px"
     }} className="container-fluid pt-1">
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-
-export const Menu = ({brand, right, left}) => (<nav className="navbar navbar-expand-md navbar-dark bg-dark">
-
-
+export const Menu = ({brand, right, left, content}) => (<nav className="navbar navbar-expand-md navbar-dark bg-dark">
+  <div className="navbar-nav text-light mr-auto">
+    {brand}
+  </div>
   <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
     <ul className="navbar-nav mr-auto">
       {left}
     </ul>
   </div>
-  <div className="mx-auto order-0">
-    {brand}
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  </div>
+
   <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
     <ul className="navbar-nav ml-auto">
       {right}
     </ul>
+    <div className="navbar-nav d-block w-100 text-light ml-auto text-right">
+      {content}
+    </div>
   </div>
+
 </nav>)
